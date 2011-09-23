c20bbf0c659651dc5b077ac78c39f2bed03eabee
Conanbatt <conanbatt@conanbatt-XPS-M1330.(none)>
2011-09-23 12:39:48 -0300

    uploaded tests and whatnot

--- README.markdown
+++ README.markdown
@@ -3,9 +3,9 @@ Kaya Open source project.
 
 Kaya will open source parts and bits of the application incrementally over time, until a stable development community is achieved.
 
-Open-source code is subject to as rigorous standards as the server. Code must have decent unit test coverage, and well documented. 
+Open-source code is subject to as rigorous standards as the server. Code must have decent unit test coverage, and well documented.
 
 Dependencies & tools
 ====================
 
-Current code is pure Javascript and can be run from within the browser.
+Current code is pure Javascript and can be run from within the browser without any installation.
---
+++ score/score_test.js
@@ -0,0 +1,84 @@
+
+//TODO this should be in the SCORE board as constants
+var BLACK = "B"
+var WHITE = "W"
+var EMPTY = "*"
+var BLACK_DEAD = "N"
+var WHITE_DEAD = "E"
+
+//STANDARD TEST BOARD SIZE IS 6x6
+
+var japanese_score = new Score("Japanese");
+var chinese_score = new Score("Chinese"); //TBD later ?. Main difference is that each stone is counted for territory
+
+test("Should count 0 points for both without stones on the board", function(){
+
+    var board = [
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"]
+                ];
+    equal(japanese_score(board).black_points,0);
+    equal(japanese_score(board).white_points,0);
+});
+
+test("Should count 35 by Japanese Rules", function(){
+
+    var board = [
+                    ["B","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"]
+                ];
+    equal(japanese_score(board).black_points,35);
+    equal(japanese_score(board).white_points,0);
+});
+
+test("Should count 0 points as all is neutral", function(){
+    var board = [
+                    ["B","W","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"],
+                    ["*","*","*","*","*","*"]
+                ];
+
+    equal(japanese_score(board).black_points,0);
+    equal(japanese_score(board).white_points,0);
+};
+
+test("Should count territory for black", function(){
+
+   var board = [
+                    ["*","B","*","*","*","*"],
+                    ["*","B","*","*","*","*"],
+                    ["*","B","*","*","*","*"],
+                    ["*","B","*","W","*","*"],
+                    ["*","B","*","*","*","*"],
+                    ["*","B","*","*","*","*"]
+                ];
+    equal(score(board).black_points,5);
+    equal(score(board).white_points,0);
+};
+
+test("Should count territory for both", function(){
+
+   var board = [
+                    ["*","B","*","W","*","*"],
+                    ["*","B","*","W","*","*"],
+                    ["*","B","*","W","*","*"],
+                    ["*","B","*","W","*","*"],
+                    ["*","B","*","W","*","*"],
+                    ["*","B","*","W","*","*"]
+                ];
+    equal(score(board).black_points,5);
+    equal(score(board).white_points,10);
+};
+
+
