ebc7914dda625f016b588c044f752d80174bdfc5
Conanbatt <conanbatt@conanbatt-XPS-M1330.(none)>
2011-09-25 02:57:26 -0300

    added data reading

--- rating/runner.rb
+++ rating/runner.rb
@@ -5,12 +5,6 @@ require 'benchmark'
 def sample_data_set
   set = []
   set << {:white_player => "pepe",:black_player => "carlos",:winner => "W"}
-  set << {:white_player => "pepe",:black_player => "carlos",:winner => "W"}
-  set << {:white_player => "pepe",:black_player => "carlos",:winner => "W"}
-  set << {:white_player => "pepe",:black_player => "carlos",:winner => "W"}
-  set << {:white_player => "pepe",:black_player => "carlos",:winner => "W"}
-  set << {:white_player => "pepe",:black_player => "carlos",:winner => "W"}
-  set << {:white_player => "pepe",:black_player => "carlos",:winner => "W"}
 end
 
 def assert(boolean)
@@ -21,7 +15,7 @@ def run_simulation(strategy, data_set)
   system = System.new(SimplePoint)
 
   time = Benchmark.measure {
-                            sample_data_set.each do |result|
+                            data_set.each do |result|
                               system.add_result(result)
                             end
                            }
@@ -29,4 +23,18 @@ def run_simulation(strategy, data_set)
 
 end
 
-run_simulation(SimplePoint, sample_data_set)
+def read_data_set(filename)
+  set = []
+  File.open(filename, "r") do |infile|
+    while (line = infile.gets)
+      w = line.split(",")[0]
+      b = line.split(",")[1]
+      winner = line.split(",")[2]
+      set << {:white_player => w, :black_player => b, :winner => winner.chomp}
+    end
+  end
+  p set
+  return set
+end
+
+run_simulation(SimplePoint, read_data_set("data/sample_data.txt"))
