f0119978e3b399bbb5b0f3e6069a1726b3bf1528
Gabriel <conanbatt@conanbatt.(none)>
2012-01-19 11:24:27 -0300

    cleanup

--- tournament/tests/base_tournament_test.rb
+++ tournament/tests/base_tournament_test.rb
@@ -1,18 +1,5 @@
 require File.expand_path("test_helper", File.dirname(__FILE__))
 
-def spawn_player_list(number)
-  list =[]
-  number.times {|n| list << Player.new(rand(8**4).to_s, list.size, "1k")}
-  list
-
-end
-
-def mock_results(tournament)
-  tournament.pairings.each do |pairing|
-    tournament.add_result(pairing.white_player, pairing.black_player, "W+R")
-  end
-end
-
 test "Should create a tournament object" do
 
   random_tournament = Organizer.create_tournament("RandomTournament",[])  
