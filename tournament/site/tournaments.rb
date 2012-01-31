5cb58b45e221a863deec1316d409b2fe7c6a19c6
Gabriel <conanbatt@conanbatt.(none)>
2012-01-31 12:50:47 -0300

    added render, basic html

--- tournament/site/tournaments.rb
+++ tournament/site/tournaments.rb
@@ -5,7 +5,7 @@ Cuba.use Rack::Session::Cookie
 Cuba.define do
   on get do
     on "home" do
-      res.write "Welcome to OpenKaya's tournament organizer"
+      res.write render("views/home.html.erb")
     end
 
     on default do
