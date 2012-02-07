983af337876b14840c766090b207c104f6a77a21
Gabriel <conanbatt@conanbatt.(none)>
2012-02-07 12:25:29 -0300

    updates on tournament showing

--- tournament/site/tournaments.rb
+++ tournament/site/tournaments.rb
@@ -1,5 +1,7 @@
 require "cuba"
 
+require File.expand_path("../organizer", File.dirname(__FILE__))
+
 Cuba.use Rack::Session::Cookie
 
 Cuba.define do
@@ -10,7 +12,12 @@ Cuba.define do
     on "create" do
       res.write render("views/create_tournament.html.erb")
     end
-
+    on "tournaments" do
+      on ":id" do |id|
+        @tournament = mock_single_elim 
+        res.write render("views/tournament.html.erb")
+      end
+    end
     on "javascripts" do
       run Rack::File.new("javascripts")
     end
@@ -22,10 +29,33 @@ Cuba.define do
 
   on post do
 
-    on "" do
-      res.write "nothing to do here"
+    on "create" do
+      on param("system"), param("participants") do |system,participants|
+        single_elim = Organizer.create_tournament(system.gsub(" ",""), create_player_list(participants.values))
+        single_elim.start_round
+        res.write single_elim.pairings
+      end
     end
 
   end
 
+end
+
+def create_player_list(players)
+  result = []
+  players.each do |p|
+    result << Player.new(p["name"],result.count,p["rank"])
+  end
+  result
+end
+
+def mock_single_elim
+  players = [{"name"=> "a","rank"=> "1k"},
+             {"name"=> "b","rank"=> "1d"},
+             {"name"=> "c","rank"=> "2d"},
+             {"name"=> "d","rank"=> "2k"}
+  ] 
+  single_elim = Organizer.create_tournament("SingleElimination",create_player_list(players))
+  single_elim.start_round
+  single_elim
 end
