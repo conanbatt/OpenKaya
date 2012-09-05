1104682fb7f77187f23deb827b23b93b630fc8da
aleski <alexis_gottlieb@hotmail.com>
2012-09-05 22:14:37 +0200

    rentame test classes in inheritance-tests.js

--- estimator/inheritance_tests.js
+++ estimator/inheritance_tests.js
@@ -1,11 +1,12 @@
 //Check js inheritance feature
-//v0.1.0
+//v0.2.0
 
 /** History
 0.1.0: creation of this test file
+0.2.0: rename test classes
 */
 
-function classParent(size) {
+function parentClass(size) {
 	//define some member variables
 	this.size = size;
 	this.value1 = "value1 from parent";
@@ -15,32 +16,32 @@ function classParent(size) {
 }
 
 //static
-classParent.StaticConstant = 1;
+parentClass.StaticConstant = 1;
 
 //static
-classParent.staticFunction1 = function(s) {
-	return "" + s + " from classParent.staticFunction1";
+parentClass.staticFunction1 = function(s) {
+	return "" + s + " from parentClass.staticFunction1";
 };
 
-classParent.prototype.getSize  = function() {
+parentClass.prototype.getSize  = function() {
 	return this.size;
 };
 
-classParent.prototype.getValue1  = function() {
+parentClass.prototype.getValue1  = function() {
 	return this.value1;
 };
 
-classParent.prototype.getValue2  = function() {
+parentClass.prototype.getValue2  = function() {
 	return this.value2;
 };
 
-classParent.prototype.doSomething  = function() {
+parentClass.prototype.doSomething  = function() {
 	return this.size*this.size;
 };
 
 
-//classChild may be defined in another file than classParent, but classParent must be loaded first
-function classChild(size, name) {
+//childClass may be defined in another file than parentClass, but parentClass must be loaded first
+function childClass(size, name) {
 	this._base_classChild.call(this, size);//call parent constructor
 	//new child member variables
 	this.name = name;
@@ -49,11 +50,11 @@ function classChild(size, name) {
 	this.constant2 = 2;
 }
 
-extendClass(classChild, classParent);//define inheritance
+extendClass(childClass, parentClass);//define inheritance
 
-classChild.prototype.doSomething  = function() {
+childClass.prototype.doSomething  = function() {
 	var parentResult = this._base_classChild.prototype.doSomething.call(this);//call parent function
-	return parentResult + classParent.StaticConstant + this.constant1 + this.constant2;
+	return parentResult + parentClass.StaticConstant + this.constant1 + this.constant2;
 };
 
 
@@ -61,8 +62,8 @@ classChild.prototype.doSomething  = function() {
 test("Constructor should be able to call parent class", function(){
 
 var expected_result = 10;
-var parent = new classParent(10);
-var child = new classChild(10, "child");
+var parent = new parentClass(10);
+var child = new childClass(10, "child");
 ok(parent.size == expected_result);
 ok(child.size == expected_result);
 
@@ -70,8 +71,8 @@ ok(child.size == expected_result);
 
 test("Child should be able to use parent member variables and can overwrite them", function(){
 
-var parent = new classParent(10);
-var child = new classChild(10, "child");
+var parent = new parentClass(10);
+var child = new childClass(10, "child");
 ok(parent.getValue1() == "value1 from parent");
 ok(child.getValue1() == "value1 from parent");
 ok(parent.getValue2() == "value2 from parent");
@@ -80,8 +81,8 @@ ok(child.getValue2() == "value2 from child");
 
 test("Child should be able to call parent functions", function(){
 
-var parent = new classParent(10);
-var child = new classChild(10, "child");
+var parent = new parentClass(10);
+var child = new childClass(10, "child");
 ok(parent.doSomething() == 100);
 ok(child.doSomething() == 104);
 });
