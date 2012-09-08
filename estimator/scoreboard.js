64e8c12050ba47e7774eebe266700f42efe5e55f
aleski <alexis_gottlieb@hotmail.com>
2012-09-08 20:05:09 +0200

    new score features

--- estimator/scoreboard.js
+++ estimator/scoreboard.js
@@ -1,11 +1,13 @@
 //Scoreboard 
 //Store a board with marked as dead/alive info
 //Provide a toggle dead/alive feature
-//v0.2.0
+//Provide count score feature
+//v0.3.0
 
 /** History
 0.1.0: creation of this file
 0.2.0: added some constants + minor code changes
+0.3.0: added count score feature
 */
 
 /** Note
@@ -24,12 +26,40 @@
 Constructor
 board param : a square double array like board = [size][size];
 */
-function ScoreBoard(board) {
+function ScoreBoard(board, komi, black_captures, white_captures) {
 //TODO: test if a double array
 	this.size = board.length;
 	this.board = ScoreBoard.cloneBoardArray(board);
+	
+	this.komi = ScoreBoard.getAsFloat(komi);
+	this.black_captures = ScoreBoard.getAsInt(black_captures);
+	this.white_captures = ScoreBoard.getAsInt(white_captures);
+	
+	this.black_estimated_territory = "unknown";
+	this.black_estimated_captures = "unknown";
+	this.white_estimated_territory = "unknown";
+	this.white_estimated_captures = "unknown";
+
 }
 
+
+ScoreBoard.getAsInt = function(s) {
+	var n =parseInt(""+s);
+	if(n == NaN) {
+		n = 0;
+	}
+	return n;
+};
+
+ScoreBoard.getAsFloat = function(s) {
+	var n =parseFloat(""+s);
+	if(n == NaN) {
+		n = 0;
+	}
+	return n;
+};
+
+/** board kind */
 //static constants
 ScoreBoard.BLACK = "B";
 ScoreBoard.WHITE = "W";
@@ -44,12 +74,23 @@ ScoreBoard.TERRITORY_BLACK = "BP";
 ScoreBoard.TERRITORY_WHITE = "WP";
 ScoreBoard.TERRITORY_SEKI = "S";
 ScoreBoard.TERRITORY_DAME = "D";
-ScoreBoard.TERRITORY_KO = "K";
+ScoreBoard.TERRITORY_KO_BLACK = "KB";
+ScoreBoard.TERRITORY_KO_WHITE = "KW";
 ScoreBoard.TERRITORY_UNKNOWN = "X";
+
+/** group status */
+ScoreBoard.STATUS_GROUP_ALIVE = "ALIVE";
+ScoreBoard.STATUS_GROUP_DEAD = "DEAD";
+ScoreBoard.STATUS_GROUP_SEKI = "SEKI";
+ScoreBoard.STATUS_GROUP_UNKNOWN = "UNKNOWN";
 
 
 
 //static
+/**
+param board: a double-array
+result : a double-array, same values as the input
+*/
 ScoreBoard.cloneBoardArray  = function(board) {
 
 	var size = board.length;
@@ -70,7 +111,9 @@ ScoreBoard.prototype.getSize  = function() {
 };
 
 
-
+/**
+returns the current board content as a double-array
+*/
 ScoreBoard.prototype.getBoardArray  = function() {
 
 	return ScoreBoard.cloneBoardArray(this.board);
@@ -78,14 +121,19 @@ ScoreBoard.prototype.getBoardArray  = function() {
 
 
 
+/**
+return a ScoreBoard copy
+*/
 ScoreBoard.prototype.clone  = function() {
 
-	return new ScoreBoard(this.board);
+	return new ScoreBoard(this.board, this.komi, this.black_captures, this.white_captures);
 };
 
 
 
-/** used by toggleAt() */
+/**
+single-array relative coordinates of neighbors for which distance == 1
+*/
 //static final
 ScoreBoard.DISTANCE1 = new Array(1, 0, -1, 0, 0, 1, 0, -1);
 
@@ -110,10 +158,16 @@ ScoreBoard.getToggleColor  = function(color) {
 };
 
 
+/**
+return
+	ScoreBoard.BLACK if the stone is black (whatever its status dead/alive etc), 
+	ScoreBoard.WHITE if the stone is white (whatever its status dead/alive etc)
+	null if territory
+*/
 //static
-ScoreBoard.getBlackOrWhite  = function(color) {
+ScoreBoard.getBlackOrWhite  = function(kind) {
 
-	switch(color) {
+	switch(kind) {
 	case ScoreBoard.BLACK:
 	case ScoreBoard.BLACK_DEAD:
 	case ScoreBoard.BLACK_ALIVE:
@@ -130,18 +184,42 @@ ScoreBoard.getBlackOrWhite  = function(color) {
 };
 
 
+ScoreBoard.prototype.getBoardKindAt  = function(i, j) {
+	return this.board[i][j];
+};
+
+
+/**
+return a boolean, true if same color
+*/
 ScoreBoard.prototype.isSameColorAt  = function(i, j, color) {
 
 	return (ScoreBoard.getBlackOrWhite(this.board[i][j]) == ScoreBoard.getBlackOrWhite(color));
 };
 
 
-ScoreBoard.prototype.isTerritory  = function(i, j) {
+/**
+return a boolean, true if territory
+*/
+ScoreBoard.prototype.isTerritoryAt  = function(i, j) {
 
 	return (ScoreBoard.getBlackOrWhite(this.board[i][j]) == null);
 };
 
 
+/**
+return a boolean, true if valid coordinates
+*/
+ScoreBoard.prototype.isInBoard  = function(i, j) {
+
+	return (i >=0 && i < this.size && j>=0 && j < this.size);
+};
+
+
+/**
+change the dead/alive status of the (i0, j0) stone
+return a double-array copy of the board
+*/
 ScoreBoard.prototype.toggleAt  = function(i0, j0) {
 
 	var color = this.board[i0][j0];
@@ -157,17 +235,16 @@ ScoreBoard.prototype.toggleAt  = function(i0, j0) {
 	coordsToCheck.push(i0);
 	coordsToCheck.push(j0);
 
-	var alreadySeen = new Object();
+	var alreadySeenThoseCoords = new Object();
 	for(;coordsToCheck.length;) {
 
 		var i = coordsToCheck.shift();
 		var j = coordsToCheck.shift();
-
 		//use a map to remember already checked stone coordinates
-		if(alreadySeen[i+1000*j] == true) {
+		if(alreadySeenThoseCoords[i+1000*j] == true) {
 			continue;
 		}
-		alreadySeen[i+1000*j] = true;
+		alreadySeenThoseCoords[i+1000*j] = true;
 		
 		//toggle if same color
 		if(!this.isSameColorAt(i, j, color)) {
@@ -180,7 +257,7 @@ ScoreBoard.prototype.toggleAt  = function(i0, j0) {
 		for(var k=0; k < ScoreBoard.DISTANCE1.length;) {
 			var ii = i+ScoreBoard.DISTANCE1[k++];
 			var jj = j+ScoreBoard.DISTANCE1[k++];
-			if(ii <0 || ii > this.size-1 || jj<0 || jj > this.size-1) {
+			if(!this.isInBoard(ii, jj)) {
 				continue; 
 			}
 			coordsToCheck.push(ii);
@@ -190,3 +267,91 @@ ScoreBoard.prototype.toggleAt  = function(i0, j0) {
 	return ScoreBoard.cloneBoardArray(this.board);
 };
 
+
+/** return group status */
+ScoreBoard.prototype.getGroupStatusAt = function(i, j) {
+	switch(this.board[i][j]) {
+	case ScoreBoard.BLACK:
+	case ScoreBoard.WHITE:
+		return ScoreBoard.STATUS_GROUP_UNKNOWN;
+	case ScoreBoard.BLACK_DEAD:
+	case ScoreBoard.WHITE_DEAD:
+		return ScoreBoard.STATUS_GROUP_DEAD;
+	case ScoreBoard.BLACK_ALIVE:
+	case ScoreBoard.WHITE_ALIVE:
+		return ScoreBoard.STATUS_GROUP_ALIVE;
+	case ScoreBoard.BLACK_SEKI:
+	case ScoreBoard.WHITE_SEKI:
+		return ScoreBoard.STATUS_GROUP_SEKI;
+	default: return null;
+	}
+};
+
+
+//prototype
+ScoreBoard.prototype.computeAnalysis  = function() {
+	//NOP
+};
+
+
+ScoreBoard.prototype.getBlackScore  = function() {
+	return this.black_estimated_territory + this.black_estimated_captures;
+};
+
+
+ScoreBoard.prototype.getWhiteScore  = function() {
+	return this.white_estimated_territory + this.white_estimated_captures + this.komi;
+};
+
+
+ScoreBoard.prototype.getGameResult = function() {
+	var bscore = this.getBlackScore();
+	var wscore = this.getWhiteScore();
+	if(bscore > wscore) {
+		return "B+"+(bscore - wscore);
+	} else if(wscore > bscore) {
+		return "W+"+(wscore - bscore);
+	} else {
+		return "Jigo!";
+	}
+};
+
+
+ScoreBoard.prototype.countJapaneseResult = function() {
+
+	this.black_estimated_territory = 0;
+	this.black_estimated_captures = 0 + this.black_captures;
+	this.white_estimated_territory = 0;
+	this.white_estimated_captures = 0 + this.white_captures;
+
+	for(var i=0;i<this.size;i++) {
+		for(var j=0;j<this.size;j++) {
+			var kind = this.getBoardKindAt(i, j);
+			if(kind == ScoreBoard.BLACK_DEAD) {
+				this.white_estimated_territory += 1;
+				this.white_estimated_captures += 1;
+			} else if(kind == ScoreBoard.WHITE_DEAD) {
+				this.black_estimated_territory += 1;
+				this.black_estimated_captures += 1;
+			} else if(kind == ScoreBoard.TERRITORY_BLACK) {
+				this.black_estimated_territory += 1;
+			} else if(kind == ScoreBoard.TERRITORY_WHITE) {
+				this.white_estimated_territory += 1;
+			} else if(kind == ScoreBoard.TERRITORY_KO_BLACK) {
+				this.black_estimated_territory += 0.5;
+			} else if(kind == ScoreBoard.TERRITORY_KO_WHITE) {
+				this.white_estimated_territory += 0.5;
+			}
+			else if(this.getGroupStatusAt(i, j) == ScoreBoard.STATUS_GROUP_DEAD) {
+				var color = ScoreBoard.getBlackOrWhite(kind);
+				if(color == ScoreBoard.BLACK) {
+					this.white_estimated_territory += 1;
+					this.white_estimated_captures += 1;
+				} else if(color == ScoreBoard.WHITE) {
+					this.black_estimated_territory += 1;
+					this.black_estimated_captures += 1;
+				}
+			}
+		}
+	}
+};
