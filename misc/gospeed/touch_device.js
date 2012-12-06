67dbfe82ed642753d1ffaae2372a589cc42ea2e0
Conanbatt <conanbatt@conanbatt-XPS-M1330>
2012-12-06 12:03:35 -0300

    added some files

---
+++ misc/gospeed/touch_device.js
@@ -0,0 +1,11 @@
+function TouchHandler(element){
+
+
+    var hover_handler = function(){ //do};
+    var click_handler = function(){ //do};
+    //Do Magic
+
+    $(element).click(click_handler);
+}
+
+
